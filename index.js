a06d7b3a4cb22428b70b0f2b4f618ef353ce6233
Clayton Ingalls <clayton.ingalls@withonevision.com>
2020-03-13 16:42:33 -0400

    Working code that finds out if item is available and sends SMS message to a specified number

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const request = require('request');
+const cheerio = require('cheerio');
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+const twilioNum = process.env.TWILIO_NUMBER;
+const sendNum = process.env.SEND_PHONE;
+
+// request("https://www.rareseeds.com/store/vegetables/live-plants/fig-olympian-2-plants-march-may-ships-prompt-weekly-as-available",
+request("https://www.rareseeds.com/store/vegetables/live-plants/pepper-black-2-plants-march-may-ships-prompt-weekly-as-available", 
+(error, response, html) => {
+  if(!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
+    const availability = $(".stock").children().last().text();
+    const available = availability.toLowerCase() === "in stock";
+
+    if(available) {
+      client.messages
+        .create({
+          body: "Black pepper vines are in stock = " + availability,
+          from: twilioNum,
+          to: clayton
+        })
+        .then(message => console.log(message.sid));
+    } else {
+      console.log("Not in stock")
+    }
+  }
+})
